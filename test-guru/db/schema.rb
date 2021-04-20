e258914317fc7e279394d5dc2b98396de41b4e36
qr.kare <katekrasnyuk@gmail.com>
2021-04-21 01:16:25 +0300

    создала модель answer

--- test-guru/db/schema.rb
+++ test-guru/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_221218) do
+ActiveRecord::Schema.define(version: 2021_04_20_221527) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body"
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -43,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_221218) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
