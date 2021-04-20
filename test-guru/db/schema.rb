57aacc7f692c5b63d9588a62fafb14470d813278
qr.kare <katekrasnyuk@gmail.com>
2021-04-21 01:57:04 +0300

    добавила дефолтные значения

--- test-guru/db/schema.rb
+++ test-guru/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_224452) do
+ActiveRecord::Schema.define(version: 2021_04_20_225537) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "correct", default: false
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
@@ -36,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_224452) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level"
+    t.integer "level", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id", null: false
