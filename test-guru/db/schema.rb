1d3b87d66f909e1d9f75c3b23d6baef2f60a0d11
qr.kare <katekrasnyuk@gmail.com>
2021-04-21 01:08:18 +0300

    создала модель user

--- test-guru/db/schema.rb
+++ test-guru/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_173117) do
+ActiveRecord::Schema.define(version: 2021_04_20_220312) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 2021_04_20_173117) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id", null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "login"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "tests", "categories"
