3c32129a93643323548ff6efd42efc7741cb6912
qr.kare <katekrasnyuk@gmail.com>
2021-04-24 16:04:57 +0300

    создала ассоциации

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: "User"
+
+  has_many :questions
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
+
   def self.test_category(category)
     joins('JOIN categories ON categories.id = tests.category_id')
       .where(categories: {title: category})
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
+
   def test_level(level)
     Test.joins('JOIN passed_tests ON passed_tests.test_id = tests.id').where(passed_tests: {user_id: id}, level: level)
   end
