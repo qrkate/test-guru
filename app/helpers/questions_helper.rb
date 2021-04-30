6863fc78ce647b12efea6d131eb789b745508b39
qr.kare <katekrasnyuk@gmail.com>
2021-04-30 19:54:08 +0300

    views

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question)
+    if question.new_record?
+      "Создать новый вопрос к #{question.test.title}"
+    else
+      "Редактировать вопрос к #{question.test.title}"
+    end
+  end
 end
