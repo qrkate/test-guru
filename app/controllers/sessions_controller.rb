99ea330a75b0c3f3d3d19841e0e07bcf553e18d3
qr.kare <katekrasnyuk@gmail.com>
2021-05-05 18:18:28 +0300

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to cookies[:path] || tests_path
-      cookies.delete(:path)
+      redirect_to  cookies.delete(:path) || tests_path
     else
       flash.now[:alert] = "Введен неверный логин или пароль!"
       render :new
