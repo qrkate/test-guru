c6dc4a90a008f13509894012d17f659305620735
qr.kare <katekrasnyuk@gmail.com>
2021-05-10 14:35:12 +0300

    after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  private
+  def after_sign_in_path_for(resource)
+     if current_user.is_a?(Admin)
+       admin_tests_path
+     else
+       tests_path
+     end
+  end
 end
