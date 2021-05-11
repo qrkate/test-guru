ff36e8812b6fe123e3bcfec86e7510d67c1af841
qr.kare <katekrasnyuk@gmail.com>
2021-05-11 23:29:36 +0300

    sessions&registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
   def after_sign_in_path_for(resource)
      if current_user.is_a?(Admin)
@@ -6,5 +8,9 @@ class ApplicationController < ActionController::Base
      else
        tests_path
      end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name])
   end
 end
