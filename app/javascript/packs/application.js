fbf4ee66066d3379aceb1010485d83bc7562a42f
qr.kare <katekrasnyuk@gmail.com>
2021-05-21 20:47:16 +0300

    переместила password_check.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 require("utilities/sorting")
-require("utilities/password_check")
+require("tool_use/password_check")
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
