9a95aadc02aa034511a236c96eadcd5fa9a8ebde
qr.kare <katekrasnyuk@gmail.com>
2021-05-25 13:25:54 +0300

    change seed&ruby_version

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 unless Test.exists?
   users = User.create!([
-    { name: 'Kate', email: 'qrkate@yandex.ru', password: 'kkk555' },
-    { name: 'Diana', email: 'di5@gmail.com', password: '123456' },
-    { name: 'Max', email: 'maxx@mail.ru', password: 'm1a2x3' }
+    { first_name: 'Kate', last_name: 'Kr', email: 'qrkate@yandex.ru', password: 'kkk555', type: 'Admin' },
+    { first_name: 'Diana', last_name: 'Di', email: 'di5@gmail.com', password: '123456' },
+    { first_name: 'Max', last_name: 'M',email: 'maxx@mail.ru', password: 'm1a2x3' }
     ])
 
   categories = Category.create!([
@@ -11,9 +11,9 @@ unless Test.exists?
     ])
 
   tests = Test.create!([
-    { title: 'Ruby', category: categories[1], author: users[0] },
+    { title: 'Ruby', category: categories[1], author: users[0], timer: 1 },
     { title: 'HTML', category: categories[0], author: users[1] },
-    { title: 'SQL', level: 2, category: categories[1], author: users[0] },
+    { title: 'SQL', level: 2, category: categories[1], author: users[0], timer: 2 },
     { title: 'CSS', level: 1, category: categories[0], author: users[2] }
     ])
 
@@ -30,22 +30,26 @@ unless Test.exists?
 
   Answer.create!([
     { body: 'Это объекты являющиеся экземплярами класса Class.', question: questions[0], correct: true },
-    { body: 'Public, private, protected.', question: questions[1] },
+    { body: 'Это объекты являющиеся классом Class.', question: questions[0]},
+    { body: 'Public, private, protected.', question: questions[1], correct: true },
+    { body: 'Public, private.', question: questions[1]},
     { body: 'Текст комментария нужно поместить внутри <!-- и -->.', question: questions[2], correct: true },
-    { body: 'Это технология, которая позволяет привязывать ссылки к фрагментам изображения.', question: questions[3] },
-    { body: 'Важен. Когда строится составной индекс, сначала индексируется первый столбец, а потом строятся индексы других столбцов, на основании главного (первого).', question: questions[4] },
+    { body: 'Текст комментария нужно поместить внутри "".', question: questions[2] },
+    { body: 'Это технология, которая позволяет привязывать ссылки к фрагментам изображения.', question: questions[3], correct: true },
+    { body: 'Это технология, которая позволяет привязывать изображения к ссылкам.', question: questions[3]},
+    { body: 'Важен.', question: questions[4], correct: true },
+    { body: 'Не важен.', question: questions[4] },
     { body: 'Однорядные функции одновременно работают только с одной строкой, а многорядные функции – с данными из нескольких строк.', question: questions[5], correct: true },
-    { body: 'Visibility или opacity.', question: questions[6] },
-    { body: 'Универсальный селектор (*).', question: questions[7] }
+    { body: 'Однорядные функции одновременно работают с данными из нескольких строк, а многорядные функции – только с одной строкой.', question: questions[5] },
+    { body: 'Visibility.', question: questions[6], correct: true },
+    { body: 'Opacity.', question: questions[6], correct: true },
+    { body: 'Универсальный селектор (*).', question: questions[7], correct: true },
+    { body: 'Универсальный селектор (/).', question: questions[7] }
     ])
 
-  PassedTest.create!([
-     { user: users[0], test: tests[0] },
-     { user: users[0], test: tests[1] },
-     { user: users[0], test: tests[2] },
-     { user: users[0], test: tests[3] },
-     { user: users[1], test: tests[2] },
-     { user: users[1], test: tests[1] },
-     { user: users[2], test: tests[3] }
-     ])
+  Badge.create!([
+    { name: 'Первая попытка', image_url: 'https://www.therpf.com/forums/attachments/publicdomainq-thumb-png.967694/', rule: 'first_try?' },
+    { name: 'Backend', image_url: 'https://w7.pngwing.com/pngs/650/298/png-transparent-front-end-web-development-web-developer-front-and-back-ends-software-developer-developer-gadget-electronics-web-application.png', rule: 'all_category?', option: 'Backend' },
+    { name: 'Новичок', image_url: 'https://w7.pngwing.com/pngs/958/741/png-transparent-mikecrack-drawing-diamantito-2-raptorgamer-painting-mikecrack-game-child-hand.png', rule: 'all_level?', option: '0' },
+    ])
 end
